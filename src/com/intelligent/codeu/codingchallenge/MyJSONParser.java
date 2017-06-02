966deff3e2cff830a4837731f3b0e1705e4b5712
anupam2704 <anupam2704@gmail.com>
2017-06-02 09:13:19 +0530

    this is commit

--- src/com/intelligent/codeu/codingchallenge/MyJSONParser.java
+++ src/com/intelligent/codeu/codingchallenge/MyJSONParser.java
@@ -17,13 +17,173 @@ import java.io.IOException;
 final class MyJSONParser implements JSONParser {
 
     private String input="";
+    private int position;
+    
   @Override
   public JSON parse(String in) throws IOException {
       
     input=in.trim();
     System.out.println(input);
+      
+    position=1;
     
-    // TODO: implement this
+    blankSpace();
+      if(input.charAt(0)=='{')
+      {
+          JSON data=parseRecursive();
+          if(position<input.length())
+          {
+              if(Pattern.compile("\\S").matcher(input.substring(position)).find())
+              {
+                  throw new IOException("Extra characters found at end");
+              }
+              else
+                  return data;
+          }
+          else
+              return data;
+      }
+      else
+      {
+          throw new IOException("Extra character found at beginnning");
+      }
+      // TODO: implement this
     return new MyJSON();
   }
+    
+    
+    
+    
+
+
+    public void blankSpace()
+    {
+        char next=input.charAt(position);
+        while(Character.isWhiteSpace(next))
+        {
+            next=input.charAt(++position);
+        }
+    }
+    
+    
+    public JSON parseSyntax throws IOException{
+        MyJSON data = new MyJSON();
+        blankSpace();
+        int numEntries=0;
+        while(input.charAt(position!='}'))
+        {
+            blankSpace();
+            if(numEntries>0)
+            {
+                if(input.charAt(position!=','))
+                {
+                    throw new IOEXception("no comma in between key value pair"+position);
+                }
+                else
+                    position++;
+            }
+            
+            blankSpace();
+
+	    	String key = getString();
+	    //	System.out.println("key: " + key);
+
+	    	blankSpace();
+
+	    	if(input.charAt(position)!=':')
+            {
+	    		//System.out.println(input.substring(index));
+	    		throw new IOException("no colon in between key and value at position " + position);
+	    	}
+	    	else
+            {
+	    		position++;
+	    	}
+
+	    	skipWhiteSpace();
+            
+            char beginningValue = input.charAt(position);
+            
+            if(beginningValue=='"')
+            {
+	        	String value = getString();
+	        	//System.out.println("string value: " + value);
+	        	numEntries++;
+
+						//checking for duplicate keys
+				if(data.strings.containsKey(key) || data.objects.containsKey(key))
+                {
+				    throw new IOException("duplicate key at position " + position);
+				}
+				else
+                {
+							//I know I can use the setString() method I defined in MyJSON.java, but I like this because it's more explicit
+	                   data.strings.put(key,value);
+				}
+	        }
+            
+            else if(beginningValue=='{')
+            {
+	           position++;
+	        //	System.out.println(input.substring(index));
+	        	numEntries++;
+				if(data.strings.containsKey(key) || data.objects.containsKey(key))
+                {
+				    throw new IOException("duplicate key at position " + position);
+				}
+				else
+                {
+	        	  data.objects.put(key, parseRecursive());
+				}
+	        }
+
+	        skipWhiteSpace();
+
+	      }
+        }
+    
+        reuturn data;
+    }
+
+    
+
+
+    public String getString() throws IOException
+    {
+	  int startIndex = position+1;
+	  String str="";
+	  if(input.charAt(position)=='"')
+      {
+  	     while(true)
+         {
+  		    position++;
+  		    if(input.charAt(position)=='\\')
+            {
+  			   char afterSlash = input.charAt(position+1);
+  			   if(afterSlash=='\\' || afterSlash=='"' || afterSlash=='n' || afterSlash=='t')
+               {
+  				  position++;
+  			   }
+  			   else
+               {
+  				  throw new IOException("invalid escaped character in string at position " + position);
+  			   }
+  		    }
+  		    else if(input.charAt(position)=='"')
+            {
+  			   str = input.substring(startIndex,position);
+  			   break;
+  			}
+  		}
+	  }
+    else 
+    {
+	 // System.out.println(input.substring(index));
+	       throw new IOException("invalid input. please try again. position " + position);
+	       throw new IOException("invalid input. please try again. position " + position);
+    }
+  	position++;
+  	return str;
+  }
+
 }
